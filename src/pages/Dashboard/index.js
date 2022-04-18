ec15fb225fe75832e2b82d9238b79ffcfdd4b51b
ErleyDiangelys <36991795+erleydiangelys@users.noreply.github.com>
2022-04-18 15:40:21 -0300

    ajuste de mensagem info

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -284,9 +284,10 @@ export default function Dashboard(){
     
               <div className='add-pedido'>
                 <input type="text" placeholder="Numero do pedido" onChange={(e)=> setAddNovoPedido(e.target.value)}/>
-                <button className="btn-add" onClick={addPedido}>Adicionar pedido</button>
-
-                <Tooltip name={<BiMessageSquareError size={20}/>} text="Digite o numero do pedido que recebeu para acompanha-lo" />
+               <div className='btn-info'> 
+                  <button className="btn-add" onClick={addPedido}>Adicionar pedido</button>
+                  <Tooltip name={<BiMessageSquareError size={20}/>} text="Digite o numero do pedido que recebeu para acompanha-lo" />
+                </div>
               </div>
 
               {loadingMore && <h3 style={{textAlign: 'center', marginTop: 15 }}>Buscando dados...</h3>}
